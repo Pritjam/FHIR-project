36194bd6688e53b9102479007ba0878b8873187b
Prithvi Jamadagni <pritjam@gmail.com>
2022-06-04 16:36:30 +0530

    form now displays the human-readable portion of a patient's data

--- fhir.js
+++ fhir.js
@@ -8,22 +8,24 @@ http.createServer(function (req, res) {
 
     var q = url.parse(req.url, true);
     if (q.pathname == '/getpatientdata') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        var form = new formidable.IncomingForm();
-        var patientid;
-        form.parse(req, function (err, fields, files) {
-            patientid = fields.patientid;
-            res.write("Patient ID: " + patientid + "<br>");
-            request('http://test.fhir.org/r2/Patient/' + patientid, { json: true }, (err, resp, body) => {
-                if (err) { return console.log(err); }
-                res.write(body.text);
-                res.end();
-
-            });
-        });
+        getPatientData(req, res);
     } else {
         res.writeHead(200, { 'content-type': 'text/html' });
         fs.createReadStream('index.html').pipe(res);
     }
 
 }).listen(8080);
+
+function getPatientData(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var patientid = fields.patientid;
+        res.write("Patient ID: " + patientid + "<br>");
+        request('http://test.fhir.org/r2/Patient/' + patientid, { json: true }, (err, resp, body) => {
+            if (err) { return console.log(err); }
+            res.write(body.text.div);
+            res.end();
+        });
+    });
+}
