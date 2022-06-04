251152cfa78eb70f16fe8fd051837e9f9cd663fc
Prithvi Jamadagni <pritjam@gmail.com>
2022-06-04 16:28:19 +0530

    updated to using node.JS

---
+++ fhir.js
@@ -0,0 +1,29 @@
+const formidable = require('formidable');
+var http = require('http');
+const fs = require('fs');
+const url = require('url');
+const request = require('request');
+
+http.createServer(function (req, res) {
+
+    var q = url.parse(req.url, true);
+    if (q.pathname == '/getpatientdata') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var form = new formidable.IncomingForm();
+        var patientid;
+        form.parse(req, function (err, fields, files) {
+            patientid = fields.patientid;
+            res.write("Patient ID: " + patientid + "<br>");
+            request('http://test.fhir.org/r2/Patient/' + patientid, { json: true }, (err, resp, body) => {
+                if (err) { return console.log(err); }
+                res.write(body.text);
+                res.end();
+
+            });
+        });
+    } else {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        fs.createReadStream('index.html').pipe(res);
+    }
+
+}).listen(8080);
