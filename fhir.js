5fe141c4ef4841256b692800996cd617dc918c93
Prithvi Jamadagni <pritjam@gmail.com>
2022-06-21 19:32:44 +0530

    added AJAX functionality. Old index is still present, but fhir.js now links to new_index.html

--- fhir.js
+++ fhir.js
@@ -6,40 +6,31 @@ const request = require('request');
 
 http.createServer(function (req, res) {
 
-    var q = url.parse(req.url, true);
-    if (q.pathname == '/getpatientdata') {
-        getPatientData(req, res);
+    var parsedURL = url.parse(req.url, true);
+    if (parsedURL.pathname == '/getpatientdata') {
+        getPatientData(req, res, parsedURL);
     } else {
         res.writeHead(200, { 'content-type': 'text/html' });
-        fs.createReadStream('index.html').pipe(res);
+        fs.createReadStream('new_index.html').pipe(res);
     }
 
 }).listen(8080);
 
-function getPatientData(req, res) {
+function getPatientData(req, res, parsedURL) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    let rawData = ''
-    req.on('data', chunk => {
-        rawData += chunk
-    })
-    
-    req.on('end', () => {
-        let parsedData = querystring.decode(rawData)
-        var patientid = parsedData.patientid;
-        res.write("Patient ID: " + patientid + "<br>");
-        let baseURL = "https://server.fire.ly/"
-        let operation = "Patient/"
+    var patientid = parsedURL.query.patientid;
+    res.write("Patient ID: " + patientid + "<br>");
+    let baseURL = "https://server.fire.ly/"
+    let operation = "Patient/"
 
-        request.get({
-            url: baseURL + operation + patientid,
-            json: true,
-            headers: { 'Accept': 'application/fhir+json' }
-        }, function (err, resp, body) {
-            if (err) { return console.log(err); }
-            res.write(body.text.div);
-            // console.log(body);
-            res.end();
-        })
+    request.get({
+        url: baseURL + operation + patientid,
+        json: true,
+        headers: { 'Accept': 'application/fhir+json' }
+    }, function (err, resp, body) {
+        if (err) { return console.log(err); }
+        res.write(body.text.div);
+        res.end();
     })
 }
